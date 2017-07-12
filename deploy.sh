c0371a7b5a3f5ebb9e457c12b35b7efe3730bde9
Antoine Motet <motet_a@epitech.eu>
2017-07-12 07:32:22 +0200

    v0.6

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,11 @@ read -p 'New version number: ' new_version
 
 sed -i "s/moteta\\/gtsearch:[0-9]*\\.[0-9]*/moteta\\/gtsearch:${new_version}/g" README.md
 
-git commit -m "v${new_version}"
+git commit -a -m "v${new_version}"
 git tag -a "v${new_version}" -m "v${new_version}"
 
 ./build.sh
 docker tag gtsearch:latest moteta/gtsearch:${new_version}
 docker push moteta/gtsearch:${new_version}
+
+git push
